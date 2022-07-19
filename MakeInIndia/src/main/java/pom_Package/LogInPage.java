526373d0b6d50c24880aa20144ef3002b7a068d6
Radhika <Radhika@192.168.0.105>
2022-07-20 01:06:30 +0530

    first commit...

---
+++ MakeInIndia/src/main/java/pom_Package/LogInPage.java
@@ -0,0 +1,10 @@
+package pom_Package;
+
+//import javax.print.attribute.standard.PrinterInfo;
+
+public class LogInPage {
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+	}
+}
